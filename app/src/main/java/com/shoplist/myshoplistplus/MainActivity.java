c1858d7e90b43951cc77d2f9fd745ef82ce719f1
JairoCruz <azaveliz@gmail.com>
2016-06-03 11:28:59 -0600

    En este commit he trabajado con la utilidad de crear un propio tema, para la app sobre escribiendo el que se brinda por defaul, el layout de activity main en diseño esta completo.

--- app/src/main/java/com/shoplist/myshoplistplus/MainActivity.java
+++ app/src/main/java/com/shoplist/myshoplistplus/MainActivity.java
@@ -1,30 +1,17 @@
 package com.shoplist.myshoplistplus;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText editText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        
     }
 
-    public void enviarMessage(View view) {
-        editText = (EditText) findViewById(R.id.txtMessage);
-        String m = editText.getText().toString();
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-        myRef.setValue(m);
-    }
+
 }
