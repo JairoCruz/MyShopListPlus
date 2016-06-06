14e30ecd8542748de2203c2a9f3e7700a598788f
JairoCruz <azaveliz@gmail.com>
2016-06-06 16:06:27 -0600

    pendiente en mostrar los datos en la vista

--- app/src/main/java/com/shoplist/myshoplistplus/activeList/ActiveListAdapter.java
+++ app/src/main/java/com/shoplist/myshoplistplus/activeList/ActiveListAdapter.java
@@ -2,9 +2,12 @@ package com.shoplist.myshoplistplus.activeList;
 
 import android.app.Activity;
 import android.view.View;
+import android.widget.TextView;
+
 
 import com.firebase.ui.database.FirebaseListAdapter;
-import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+import com.shoplist.myshoplistplus.R;
 import com.shoplist.myshoplistplus.model.ShoppingList;
 
 /**
@@ -13,12 +16,27 @@ import com.shoplist.myshoplistplus.model.ShoppingList;
 
 // Populates the list_view_active_list inside ShoppingListFragment
 public class ActiveListAdapter extends FirebaseListAdapter<ShoppingList> {
-    public ActiveListAdapter(Activity activity, Class<ShoppingList> modelClass, int modelLayout, DatabaseReference ref) {
+
+    private String mEncodedEmail;
+
+    /**
+     * Public constructor that initializes private instance variables when adapter is created
+     */
+    public ActiveListAdapter(Activity activity, Class<ShoppingList> modelClass, int modelLayout, Query ref, String encodedEmail) {
         super(activity, modelClass, modelLayout, ref);
+        this.mEncodedEmail = encodedEmail;
+        this.mActivity = activity;
     }
 
+    /**
+     * Protected method that populates the view attached to the adapter (list_view_active_lists)
+     * with items inflated from single_active_list.xml
+     * populateView also handles data changes and updates the listView accordingly
+     */
     @Override
     protected void populateView(View v, ShoppingList model, int position) {
-
+        TextView textViewListName = (TextView) v.findViewById(R.id.text_view_list_name);
+        final TextView textViewCreatedByUser = (TextView) v.findViewById(R.id.text_view_created_by_user);
+        final TextView textViewUsersShopping = (TextView) v.findViewById(R.id.text_view_people_shopping_count);
     }
 }
--- app/src/main/java/com/shoplist/myshoplistplus/activeList/ShoppingListFragment.java
+++ app/src/main/java/com/shoplist/myshoplistplus/activeList/ShoppingListFragment.java
@@ -3,12 +3,20 @@ package com.shoplist.myshoplistplus.activeList;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.shoplist.myshoplistplus.R;
+import com.shoplist.myshoplistplus.model.ShoppingList;
 import com.shoplist.myshoplistplus.utils.Constans;
 
 
@@ -20,6 +28,8 @@ public class ShoppingListFragment extends Fragment {
     private String mEncodeEmail;
     // private ActiveListAdapter mActiveListAdapter;
     private ListView mListView;
+    private ActiveListAdapter mActiveListAdapter;
+    private TextView mTextViewListName;
 
 
 
@@ -52,6 +62,27 @@ public class ShoppingListFragment extends Fragment {
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+
+        /**
+         * Create the adapter with selected sort order
+         */
+        // mActiveListAdapter = new ActiveListAdapter(getActivity(), ShoppingList.class, R.layout.single_active_list, orderedActivieUserListsRef, mEncodeEmail);
+
+        /**
+         *  Set the adapter to the mListView
+         * */
+      //  mListView.setAdapter(mActiveListAdapter);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+       // mActiveListAdapter.cleanup();
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         /*// Inflate the layout for this fragment
@@ -63,11 +94,30 @@ public class ShoppingListFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_shopping_list, container, false);
         initializeScreen(rootView);
 
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("listName");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String listName = dataSnapshot.getValue(String.class);
+                Log.d("error", "Value is:" + listName);
+                mTextViewListName.setText(listName);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("error1","Failed to read value", databaseError.toException());
+            }
+        });
+
         return rootView;
     }
 
     private void initializeScreen(View rootView) {
         mListView = (ListView) rootView.findViewById(R.id.list_view_active_list);
+        mTextViewListName = (TextView) rootView.findViewById(R.id.text_view_list_name);
+        mTextViewListName.setText("hola");
     }
 
 }
