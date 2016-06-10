b57dac1583c9ed54dac1b5493eac3c14d7b2b971
JairoCruz <azaveliz@gmail.com>
2016-06-10 14:21:25 -0600

    FragmentMeals el diseño esta listo,.

--- app/src/main/java/com/shoplist/myshoplistplus/meals/MealsFragment.java
+++ app/src/main/java/com/shoplist/myshoplistplus/meals/MealsFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import com.shoplist.myshoplistplus.R;
 
@@ -14,9 +16,11 @@ import com.shoplist.myshoplistplus.R;
  * A simple {@link Fragment} subclass.
  */
 public class MealsFragment extends Fragment {
+    private ListView mListView;
 
-
-
+    /**
+     * Este es el cascaron de este fragment el cual implementa un listview. este listview se le agrego un OnItemClickListener
+     */
 
     public MealsFragment() {
         // Required empty public constructor
@@ -30,11 +34,30 @@ public class MealsFragment extends Fragment {
     }
 
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_meals, container, false);
+        /* Inflate the layout for this fragment */
+        View rootView = inflater.inflate(R.layout.fragment_meals,container,false);
+
+        /* Link layout elments from XML and setup the toolbar */
+        initializeScreen(rootView);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+
+        return rootView;
+    }
+
+    public void initializeScreen(View rootView){
+        mListView = (ListView) rootView.findViewById(R.id.list_view_meals_list);
+        View footer = getActivity().getLayoutInflater().inflate(R.layout.footer_empty,null);
+        mListView.addFooterView(footer);
     }
 
 }
